c7dda6fdde650b6ed3cc77c7f26945cdc9dd769b
aaronprograms <aaron.eh.son@gmail.com>
2020-02-03 23:21:11 -0500

    Solution

--- 10-days-of-javascript/day-2-conditional-statements-if-else.js
+++ 10-days-of-javascript/day-2-conditional-statements-if-else.js
@@ -25,7 +25,26 @@ function readLine() {
 function getGrade(score) {
     let grade;
     // Write your code here
-    
+
+    switch(true){
+        case score > 25:
+            grade = "A";
+            break;
+        case score > 20:
+            grade = "B";
+            break;
+        case score > 15:
+            grade = "C";
+            break;
+        case score > 10:
+            grade = "D";
+            break;
+        case score > 5:
+            grade = "E";
+            break;
+        default:
+            grade = "F";
+    }
     return grade;
 }
 
